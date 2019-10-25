0a74e928bbf91b9c3dfc388b39197328f005d58c
Robert Szlezak <robertszlezak@gmail.com>
2019-10-25 10:36:51 +0200

    Fixed tsconfig

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,27 @@
-import { useEffect, useRef, useState } from "react";
-import debounce from "lodash.debounce";
+import { useEffect, useRef, useState } from 'react';
+import debounce from 'lodash.debounce';
 
-const usePersist = (key: string, storageType: "localStorage"|"sessionStorage", debounceLimit: number) => {
+const usePersist = (
+  key: string,
+  storageType?: 'localStorage' | 'sessionStorage',
+  debounceLimit?: number,
+): [any, Function] => {
   const DEBOUNCE_LIMIT = debounceLimit || 250;
-  const mounted = useRef();
+  const STORAGE_TYPE = storageType || 'localStorage';
+
+  const mounted = useRef<boolean>();
   const [data, setData] = useState({});
 
-  const { setItem, getItem } = window[storageType] || window.localStorage || {};
-
   const persist = debounce(dataToStore => {
-    setItem(key, JSON.stringify(dataToStore));
+    window[STORAGE_TYPE].setItem(key, JSON.stringify(dataToStore));
   }, DEBOUNCE_LIMIT);
 
   useEffect(() => {
     if (!mounted.current) {
-      const storedData = getItem(key);
+      const storedData = window[STORAGE_TYPE].getItem(key);
 
-      storedData && setData(JSON.parse(storedData))
       mounted.current = true;
+      storedData && setData(JSON.parse(storedData));
     } else {
       persist(data);
     }
